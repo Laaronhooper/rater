d60ed0ec742ad83ca274b54e335301689bb281dd
Luke Hooper <laaronhooper@gmail.com>
2021-11-29 12:37:03 +1100

    model creation and seed data

---
+++ app/models/buisness.rb
@@ -0,0 +1,11 @@
+class Buisness < ApplicationRecord
+  has_many :reviews
+
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  def average_score
+    reviews.average(:score).round(2).to_f
+  end
+end
